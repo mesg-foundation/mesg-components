6d723a1dc5cc6bf9354629c17f3d4aa0df2e0c02
Benz <benz@yellow.com>
2019-07-18 14:20:25 +0700

    Set component variable

--- components/AtdCard/AtdCard.stories.js
+++ components/AtdCard/AtdCard.stories.js
@@ -1,9 +1,25 @@
 import { storiesOf } from '@storybook/vue'
 import AtdCard from './AtdCard.vue'
 
-storiesOf('AtdCard', module).add('normal', () => ({
-  components: { AtdCard },
-  template: `
-      <AtdCard/>
-    `
-}))
+storiesOf('AtdCard', module)
+  .add('default', () => ({
+    components: { AtdCard },
+    template: `
+      <AtdCard :card="card"/>
+    `,
+    data: () => ({ card: 'card' })
+  }))
+  .add('outline', () => ({
+    components: { AtdCard },
+    template: `
+      <AtdCard :card="card"/>
+    `,
+    data: () => ({ card: 'card-base' })
+  }))
+  .add('outline-base', () => ({
+    components: { AtdCard },
+    template: `
+      <AtdCard :card="card" />
+    `,
+    data: () => ({ card: 'card-base-bottom' })
+  }))
