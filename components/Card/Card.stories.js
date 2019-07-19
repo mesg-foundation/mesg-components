fe381391efb10b6871221debbb9708ea80ea70b4
Benz <benz@yellow.com>
2019-07-19 11:28:18 +0700

    Move card component from mesg.com to mesg components

---
+++ components/Card/Card.stories.js
@@ -0,0 +1,20 @@
+import { storiesOf } from '@storybook/vue'
+import Card from './Card.vue'
+
+storiesOf('Card', module)
+  .add('default', () => ({
+    components: { Card },
+    template: `<Card>TEST</Card>`
+  }))
+  .add('without shadow ', () => ({
+    components: { Card },
+    template: `<Card noShadow >TEST</Card>`
+  }))
+  .add('with bordered ', () => ({
+    components: { Card },
+    template: `<Card bordered>TEST</Card>`
+  }))
+  .add('with bordered thin ', () => ({
+    components: { Card },
+    template: `<Card bordered thin>TEST</Card>`
+  }))
