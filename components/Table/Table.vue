4aa774dbb1f8afd501aa31cb4e73a822743f0224
Dev-Yellow-Wat <wat@yellow.com>
2019-07-22 15:21:11 +0700

    :tada: Initial table component.

---
+++ components/Table/Table.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-data-table :headers="headers" :items="items">
+      <template v-slot:items="props">
+        <td v-for="(header,i) in headers" :key="i">{{ props.item[header.value] }}</td>
+      </template>
+    </v-data-table>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mytable", 
+  props: {
+    headers: {
+      type: Array,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
