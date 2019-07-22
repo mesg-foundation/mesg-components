499b4c481474baacf5226dd0f469d777afaaf0e4
Benz <benz@yellow.com>
2019-07-22 18:12:18 +0700

    Display text value as center

--- components/Chart/doughtnut/DoughnutChartConfig.js
+++ components/Chart/doughtnut/DoughnutChartConfig.js
@@ -1,1 +1,28 @@
-import { Doughnut } from 'vue-chartjs'
+import { Doughnut, mixins } from 'vue-chartjs'
+
+Chart.pluginService.register({
+  beforeDraw: function(chart) {
+    let width = chart.chart.width
+    let height = chart.chart.height
+    let ctx = chart.chart.ctx
+    ctx.restore()
+    let fontSize = (height / 114).toFixed(2)
+    ctx.font = fontSize + 'em sans-serif'
+    ctx.textBaseline = 'middle'
+    ctx.fillStyle = chart.config.options.centercolor
+    const text = chart.config.options.elements.center.text,
+      textX = Math.round((width - ctx.measureText(text).width) / 2),
+      textY = height / 2
+    ctx.fillText(text, textX, textY)
+    ctx.save()
+  }
+})
+
+export default {
+  extends: Doughnut,
+  mixins: [mixins.reactiveProp],
+  props: ['options'],
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
