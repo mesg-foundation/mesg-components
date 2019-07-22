0f34661bd8857edf1fe5df6220b0d2145c6f5b51
Dev-Yellow-Wat <wat@yellow.com>
2019-07-22 17:30:44 +0700

    Add link image and collapse.

--- components/Table/Table.vue
+++ components/Table/Table.vue
@@ -2,15 +2,23 @@
   <div>
     <v-data-table :headers="headers" :items="items">
       <template v-slot:items="props">
-        <td v-for="(header,i) in headers" :key="i">{{ props.item[header.value] }}</td>
+        <td v-for="(header,i) in headers" :key="i">
+          <a v-if="header.link" :href="props.item[header.value]" target="_blank">
+            <img :src="src(header.icon)" :srcset="srcset(header.icon)" />
+          </a>
+          <div v-else-if="header.collapse" class="collapse">
+            <img :src="src(header.icon)" />
+          </div>
+          <div v-else>{{ props.item[header.value] }}</div>
+        </td>
       </template>
-    </v-data-table>    
+    </v-data-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: "mytable", 
+  name: "mytable",
   props: {
     headers: {
       type: Array,
@@ -20,9 +28,30 @@ export default {
       type: Array,
       required: true
     }
+  },
+  computed: {},
+  methods: {
+    src(name) {
+      return require(`../../assets/images/${name}.png`);
+    },
+    srcset(name) {
+      const img2x = require(`../../assets/images/${name}@2x.png`);
+      const img3x = require(`../../assets/images/${name}@3x.png`);
+      return `${img2x}, ${img3x}`;
+    }
   }
 };
 </script>
 
-<style>
+<style scoped>
+img {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.collapse {
+  cursor: pointer;
+  text-align: center;
+  margin: 20px;
+}
 </style>
