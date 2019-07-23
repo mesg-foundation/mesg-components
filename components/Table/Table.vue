d3802fa87b6ac9d16a62bd5fb8a087037d912478
Dev-Yellow-Wat <wat@yellow.com>
2019-07-23 10:53:07 +0700

    Set column type (date, number and percentage)

--- components/Table/Table.vue
+++ components/Table/Table.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
-    <v-data-table :headers="headers" :items="items">
+    <v-data-table :headers="headers" :items="items" hide-actions item-key="link" :expand="expand">
       <template v-slot:items="props">
-        <td v-for="(header,i) in headers" :key="i">
-          <a v-if="header.link" :href="props.item[header.value]" target="_blank">
-            <img :src="src(header.icon)" :srcset="srcset(header.icon)" />
-          </a>
-          <div v-else-if="header.collapse" class="collapse">
-            <img :src="src(header.icon)" />
-          </div>
-          <div v-else>{{ props.item[header.value] }}</div>
-        </td>
+        <tr @click="props.expanded = !props.expanded">
+          <td v-for="(header,i) in headers" :key="i">
+            <a v-if="header.link" :href="props.item[header.value]" target="_blank">
+              <img :src="src(header.icon)" :srcset="srcset(header.icon)" />
+            </a>
+            <div v-else-if="header.collapse" class="collapse">
+              <img :src="src(header.icon)" />
+            </div>
+            <div v-else-if="header.type=='number'">{{ props.item[header.value] | number('0,0.00') }}</div>
+            <div v-else-if="header.type=='percentage'" >{{ props.item[header.value] | number('0,0.00') }}%</div>          
+            <div v-else>{{ props.item[header.value] }}</div>
+          </td>
+        </tr>
+      </template>
+
+      <template v-slot:expand="props">
+        <v-card flat>
+          <v-card-text>Peek-a-boo!</v-card-text>
+        </v-card>
       </template>
     </v-data-table>
   </div>
@@ -19,6 +29,11 @@
 <script>
 export default {
   name: "mytable",
+  data() {
+    return {
+      expand: false
+    };
+  },
   props: {
     headers: {
       type: Array,
