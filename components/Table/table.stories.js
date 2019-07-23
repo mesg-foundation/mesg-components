5fc6b0d641c34b61bdc6a98e30193052eb1c552d
Dev-Yellow-Wat <wat@yellow.com>
2019-07-23 17:45:18 +0700

    Remove unused plugin, v-app and v-content

--- components/Table/table.stories.js
+++ components/Table/table.stories.js
@@ -1,21 +1,14 @@
 import { storiesOf } from '@storybook/vue'
 import Table from './Table.vue'
-import '../../plugin/vuetify'
-import '../../plugin/filter.number'
-import '../../plugin/filter.date'
 
 storiesOf('Table', module)
   .add('default', () => ({
     components: { Table },
     template: `
-  <v-app>
-    <v-content>
       <Table
         :headers="headers"
         :items="items"
-      ></Table>
-    </v-content>
-  </v-app>`,
+      ></Table>`,
     data: () => ({
       headers: [
         {
@@ -116,41 +109,35 @@ storiesOf('Table', module)
   .add('distribution', () => ({
     components: { Table },
     template: `
-  <v-app>
-    <v-content>
       <Table
         :headers="headers"
         :items="items"
-      ></Table>
-    </v-content>
-  </v-app>`,
+      ></Table>`,
     data: () => ({
       headers: [
-        { text: 'Date', align: 'left', value: 'date',type:'date' },
-        { text: 'Trade Volume', align: 'left', value: 'tradeVolume', type:'number' },
-        { text: 'Token Release', align: 'left', value: 'tokenRelease' , type:'number'},
-        { text: 'Percentage', align: 'left', value: 'percentage', type:'percentage' },
-        { text: 'Link', align: 'left', value: 'link', link: true, icon: 'etherscan' },
-        { text: 'Collapse', align: 'center', sortable: false, value: 'collapse', collapse: true, icon: 'down-purple' }
+        { text: 'Date', align: 'left', value: 'date', type: 'date' },
+        { text: 'Trade Volume', align: 'left', value: 'tradeVolume', type: 'number', decimalDigit: 4 },
+        { text: 'Token Release', align: 'left', value: 'tokenRelease', type: 'number', decimalDigit: 4 },
+        { text: 'Percentage', align: 'left', value: 'percentage', type: 'percentage' },
+        { text: 'Link', align: 'center', value: 'link', link: true, icon: 'etherscan' },
+        { text: '   ', align: 'center', sortable: false, value: 'collapse', collapse: true, icon: 'down-purple' }
       ],
       items: [
         {
           date: 'Fri, 05 Jul 2019 07:00:12 GMT',
-          tradeVolume: 3598919.08,
-          tokenRelease: 251924.34,
+          tradeVolume: 3598919.08989,
+          tokenRelease: 251924.3499899889,
           percentage: '7',
           link: 'https://etherscan.io/tx/0xa079c2ef2286e3e8b68745e0a35a17aa22b392e30189c2e5a506bbfdb26babd6',
-          collapse: '',
-          count: 1
+          collapse: ''
         },
         {
           date: 'Sat, 06 Jul 2019 07:00:12 GMT',
-          tradeVolume: 4598920.08,
-          tokenRelease: 351924.34,
+          tradeVolume: 4598920.087655,
+          tokenRelease: 351924.349876,
           percentage: '6',
           link: 'https://etherscan.io/tx/0x31de0dfd21be29496eacc026e001b6dede73c4a31254afc837fe7693a2158ab1',
-          collapse: '',
-          count:2
+          collapse: ''
         }
       ]
     })
