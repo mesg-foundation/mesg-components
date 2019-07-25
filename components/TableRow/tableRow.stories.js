c534aac597ef0d2e6ef19c5c62f5a345218a632a
Dev-Yellow-Wat <wat@yellow.com>
2019-07-25 16:45:22 +0700

    Edit json data.

--- components/TableRow/tableRow.stories.js
+++ components/TableRow/tableRow.stories.js
@@ -1,15 +1,39 @@
 import { storiesOf } from '@storybook/vue'
-import Table from './TableRow.vue'
+import List from './TableRow.vue'
 import '../../assets/style.css'
 
 storiesOf('SubTable', module)
   .add('default', () => ({
-    components: { Table },
+    components: { List },
     template: `
-      <Table
+      <List
         :distributions="distributions"
         :title="title"
-      ></Table>`,
+      >      
+      </List>`,
+    data: () => ({
+      title:'Distribution of the MESG Token released',
+      distributions:[
+        {name:'sale', amount:'1234.48', percentage:2},
+        {name:'partner', amount:'7339366998.48', percentage:1},
+        {name:'founder', amount:'146796.24', percentage:4},
+        {name:'reserve', amount:'0.00', percentage:0}
+      ]
+    })
+  }))
+  .add('custom', () => ({
+    components: { List },
+    template: `
+      <List
+        :distributions="distributions"
+        :title="title"
+      >
+      <template slot-scope="{ item }">      
+        <div>Custom-Name: {{item.name }} </div>
+        <div>Custom-Amount: {{item.amount }} </div>
+        <div>Custom-Percentage: {{item.percentage }} </div>
+      </template>
+      </List>`,
     data: () => ({
       title:'Distribution of the MESG Token released',
       distributions:[
