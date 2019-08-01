20f0e1ae56190a1121f98230af60b3edd4ad82a9
Dev-Yellow-Wat <wat@yellow.com>
2019-08-01 18:40:00 +0700

    Rename component, change element (from dev to nav) and install side menu package.

---
+++ components/SideMenu/SideMenu.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav class="sidebar">
+    <template v-for="(item,i) in items">
+      <slot :name="item.key" :item="item">
+        <a :href="item.href" class="link-secondary" :key="i">
+          <i class="chevron right"></i>
+          {{ item.text }}
+        </a>
+      </slot>
+    </template>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Sider",
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  flex-flow: column wrap;
+}
+
+.sidebar a {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  font-size: 17px;
+  font-weight: 700;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+}
+
+a {
+  font-family: "Open Sans", sans-serif;
+  font-size: 17px;
+  font-weight: 700;
+  color: var(--Purple-3);
+  text-decoration: none;
+}
+
+i {
+  margin-right: 10px;
+  font-size: 12px;
+}
+
+.link-secondary:hover {
+  opacity: 0.7;
+  transition: 0.1s ease;
+}
+
+.chevron::before {
+  border-style: solid;
+  border-width: 0.12em 0.12em 0 0;
+  content: "";
+  display: inline-block;
+  height: 0.45em;
+  left: 0.15em;
+  position: relative;
+  top: 0.15em;
+  transform: rotate(-45deg);
+  vertical-align: super;
+  width: 0.45em;
+  font-size: 12px;
+}
+
+.chevron.right:before {
+  left: 0;
+  transform: rotate(45deg);
+}
+
+.chevron.bottom:before {
+  top: 0;
+  transform: rotate(135deg);
+}
+
+.chevron.left:before {
+  left: 0.25em;
+  transform: rotate(-135deg);
+}
+</style>
---
+++ storybook/stories/SideMenu.stories.js
@@ -0,0 +1,44 @@
+import { storiesOf } from '@storybook/vue'
+import Sider from '@mesg-components/side-menu/SideMenu'
+
+import '../style.css'
+
+const items = [
+  { key: 'general', text: 'General', href: '#' },
+  { key: 'gettingst', text: 'Getting Start', href: '#' },
+  { key: 'transaction', text: 'Transactions', href: '#' },
+  { key: 'compatibility', text: 'Compatibility', href: '#' },
+  { key: 'community', text: 'Community', href: '#' },
+  { key: 'businesss', text: 'Business and Strategy', href: '#' },
+  { key: 'development', text: 'Development', href: '#' }
+]
+
+storiesOf('SideMenu', module)
+.add('default', () => ({
+  components: { Sider },
+  template: `<Sider :items="items"/>`,
+  data: () => ({
+    items
+  })
+}))
+.add('custom', () => ({
+  components: { Sider },
+  template: `<Sider :items="items">
+      <template v-slot:general="{ item }">
+        <a :href="item.href" class="link-secondary">        
+          <i class="chevron right"></i>
+          <span>[Custom]-general</span>
+        </a>  
+      </template>
+
+      <template v-slot:businesss="{ item }">
+        <a :href="item.href" class="link-secondary">        
+          <i class="chevron right"></i>
+          <span>[Custom]-{{item.text}}</span>
+        </a>  
+      </template>
+    </Sider>`,
+  data: () => ({
+    items
+  })
+}))
