1b4d84bc87e857180a9ac91dd1da3b8942004cbc
Benz <benz@yellow.com>
2019-08-06 11:41:24 +0700

    Set props circular

--- components/Progressbar/Circular/CircularProgressbar.vue
+++ components/Progressbar/Circular/CircularProgressbar.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>Circular ProgressBar</div>
+  <div class="circular" :style="{width,height}"></div>
 </template>
 
 <script>
 export default {
-  name: 'CircularProgressbar'
+  name: 'CircularProgressbar',
+  props: {
+    width: { type: String, default: '50px' },
+    height: { type: String, default: '50px' }
+  }
 }
 </script>
 
 <style scoped>
+.circular {
+  border: 0.5rem solid var(--Purple-3);
+  border-top-color: rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  animation: 1s rotate linear infinite;
+  position: relative;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
