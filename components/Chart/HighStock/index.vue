e2f170732115b6b7b7970ab4504578cd3459e766
Benz <benz@yellow.com>
2019-08-29 15:23:35 +0700

    Set option hight stock chart

--- components/Chart/HighStock/index.vue
+++ components/Chart/HighStock/index.vue
@@ -10,7 +10,10 @@ export default {
     rangeSelectorOption: { type: Object },
     yAxisOption: { type: Object | Array },
     optionLegend: { type: Object },
-    optionsTooltip: { type: Object }
+    optionsTooltip: { type: Object },
+    optionsPlot: { type: Object },
+    chartOption: { type: Object },
+    noNavigator: { type: Boolean, default: false }
   },
   mixins: [chart],
   data() {
@@ -20,11 +23,14 @@ export default {
   },
   mounted() {
     this.chart = new Highcharts.stockChart(this.$el, {
+      chart: this.chartOption || {},
       rangeSelector: this.rangeSelectorOption,
       yAxis: this.yAxisOption,
       legend: this.legendOption(!this.noLegend, this.optionLegend || {}),
       tooltip: this.tooltipOption(!this.noTooltip, this.optionsTooltip || { valueDecimals: 2, split: true }),
       credits: this.creditsOption(this.credit),
+      navigator: { enabled: !this.noNavigator },
+      plotOptions: this.optionsPlot || {},
       series: this.items
     })
   }
