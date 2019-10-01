a1160cffa688484081363545613fd1a5eb5b748d
Benz <ruthamnong.thotsaphon@gmail.com>
2019-10-01 13:51:11 +0700

    Add Header components

---
+++ packages/Header/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="header" pt3 pb3 mb3>
+    <div v-if="!noPicture" class="container" flex row mobile-column-reverse align-center space-between wrap>
+      <div half>
+        <h1 mb1>{{title}}</h1>
+        <p mb2>{{description}}</p>
+        <slot v-if="$slots.default" />
+      </div>
+      <div half>
+        <img v-if="imgUrl" :src="imgUrl" :alt="title" />
+        <slot name="items"></slot>
+      </div>
+    </div>
+    <div v-else class="container">
+      <div>
+        <h1 mb1>{{title}}</h1>
+        <p mb2>{{description}}</p>
+      </div>
+      <slot v-if="$slots.default" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: { type: String },
+    description: { type: String },
+    noPicture: { type: Boolean, default: false },
+    imgUrl: { type: String }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@mesg-components/theme/index.scss';
+
+#header {
+  position: relative;
+}
+#header::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: calc(100% + 200px);
+  background-image: linear-gradient(to top, var(--light-grey), var(--White));
+  transform: translateY(-35%) skewY(-8deg);
+  z-index: -1;
+}
+p {
+  font-size: 20px;
+}
+img {
+  height: auto;
+  max-height: calc(var(--width) / 2);
+}
+@media only screen and (max-width: $mobile-breakpoint) {
+  #header::before {
+    height: calc(100% + 400px);
+  }
+  img {
+    margin-bottom: calc(var(--margin) * 2);
+  }
+}
+</style>
