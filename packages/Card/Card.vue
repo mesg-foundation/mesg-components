8c02bd0566559171ecde36e0a6380d30ccea159e
Benz <ruthamnong.thotsaphon@gmail.com>
2019-10-03 18:01:34 +0700

    Update card component set background

--- packages/Card/Card.vue
+++ packages/Card/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card" :class="{ bordered, thin, shadow: !noShadow }" :style="{borderBottomColor: borderColor}">
+  <div class="card" :class="{ bordered, thin, shadow: !noShadow,background }" :style="{borderBottomColor: borderColor, backgroundColor: bgColor,color: textColor}">
     <slot />
   </div>
 </template>
@@ -10,8 +10,11 @@ export default {
   props: {
     bordered: { type: Boolean, default: false },
     thin: { type: Boolean, default: false },
+    background: { type: Boolean, default: false },
     noShadow: { type: Boolean, default: false },
-    borderColor: { type: String, default: undefined }
+    borderColor: { type: String, default: undefined },
+    bgColor: { type: String, default: undefined },
+    textColor: { type: String, default: undefined }
   }
 }
 </script>
@@ -22,32 +25,39 @@ export default {
 
 .card {
   border-radius: 6px;
-  background-color: $White;
+  background-color: var(--White);
   overflow: hidden;
+  padding: var(--margin);
 }
 .card.shadow {
   box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2);
 }
+
+.background {
+  background-color: var(--light-grey);
+}
+
 .card.bordered {
-  border: solid 1px $primary-very-light;
+  border: solid 1px var(--primary-very-light);
   border-bottom-width: 6px;
-  border-bottom-color: $primary;
+  border-bottom-color: var(--primary);
   box-shadow: none;
 }
+
 .card.bordered:hover {
   transition: 0.2s ease;
-  border: solid 1px $primary;
+  border: solid 1px var(--primary);
   border-bottom-width: 6px;
-  border-bottom-color: $primary;
-  box-shadow: 0 0 0 1px $primary inset;
+  border-bottom-color: var(--primary);
+  box-shadow: 0 0 0 1px var(--primary) inset;
 }
 .card.bordered.thin {
   border: solid 2px transparent;
-  box-shadow: 0 0 0 1px $primary-very-light inset;
+  box-shadow: 0 0 0 1px var(--primary-very-light) inset;
 }
 .card.bordered.thin:hover {
   transition: 0.2s ease;
-  border-color: $primary;
+  border-color: var(--primary);
   box-shadow: none;
 }
 </style>
--- storybook/stories/Card.stories.js
+++ storybook/stories/Card.stories.js
@@ -18,3 +18,15 @@ storiesOf('Card', module)
     components: { Card },
     template: `<Card bordered thin>TEST</Card>`
   }))
+  .add('with background ', () => ({
+    components: { Card },
+    template: `<div class="container"><Card background no-shadow >TEST</Card></div>`
+  }))
+  .add('with background custom color ', () => ({
+    components: { Card },
+    template: `<div class="container"><Card bgColor="yellow" no-shadow >TEST</Card></div>`
+  }))
+  .add('with background and text custom color ', () => ({
+    components: { Card },
+    template: `<div class="container"><Card bgColor="black" no-shadow text-color="#fff">TEST</Card></div>`
+  }))
