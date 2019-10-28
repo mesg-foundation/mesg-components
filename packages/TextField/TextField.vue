f476396f2c5df8fe209ac782c9264b424fbb8a35
Benz <ruthamnong.thotsaphon@gmail.com>
2019-10-28 14:53:05 +0700

    Add textfield components

---
+++ packages/TextField/TextField.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>TextField</div>
+</template>
+
+<script>
+export default {
+  name: 'TextField',
+  props: {
+    type: { type: String, default: 'text' },
+    label: { type: String, required: true },
+    placeholder: { type: String, required: true }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@mesg-components/theme/_variables';
+</style>
