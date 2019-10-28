89588d372daa7e32e2b9601ce41386dcc8084e44
Benz <ruthamnong.thotsaphon@gmail.com>
2019-10-28 16:25:30 +0700

    Update textfield

--- packages/TextField/TextField.vue
+++ packages/TextField/TextField.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>TextField</div>
+  <div>
+    <p class="paragraph">{{label}}</p>
+    <input class="input-text" :type="type" :placeholder="placeholder" v-bind:value="value" v-on:input="$emit('input', $event.target.value)" />
+  </div>
 </template>
 
 <script>
@@ -8,11 +11,48 @@ export default {
   props: {
     type: { type: String, default: 'text' },
     label: { type: String, required: true },
-    placeholder: { type: String, required: true }
+    placeholder: { type: String, required: true },
+    value: { type: [String, Number], required: true }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@mesg-components/theme/_variables';
+
+.paragraph {
+  font-size: 15px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: $primary-dark;
+  margin-bottom: 10px;
+}
+
+.input-text {
+  border-radius: 3px;
+  height: 50px;
+  border: solid 1px $primary-very-light;
+  background-color: $white;
+  width: 100%;
+  min-width: 100%;
+}
+
+input {
+  padding: 15px $margin;
+  font-size: 15px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: $dark-grey;
+}
+
+input:focus {
+  outline: none;
+  box-shadow: 0 0 0 1px $primary-very-light inset;
+}
 </style>
