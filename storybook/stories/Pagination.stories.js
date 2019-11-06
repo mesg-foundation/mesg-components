42329cdadc29379f11357d0451316589870ae4bb
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-06 18:04:43 +0700

    Remove unused variables on storybook

--- storybook/stories/Pagination.stories.js
+++ storybook/stories/Pagination.stories.js
@@ -1,14 +1,6 @@
 import { storiesOf } from '@storybook/vue'
 import Pagination from '@mesg-components/pagination'
 
-const headers = [
-  { key: 'date', text: 'Date', align: 'left', value: 'date' },
-  { key: 'volume', text: 'Traded Volume', align: 'center', value: 'tradeVolume' },
-  { key: 'token', text: 'Token Release', align: 'center', value: 'tokenRelease' },
-  { key: 'percentage', text: 'Percentage', align: 'center', value: 'percentage' },
-  { key: 'link', text: 'Link', align: 'center', value: 'link' }
-]
-
 const items = new Array(90).fill(null).map((x, i) => {
   const percentage = Math.random() * 10
   const volume = Math.random() * 5000000
@@ -27,29 +19,19 @@ storiesOf('Pagination', module)
     components: { Pagination },
     template: `
     <div class="container">
-      <Pagination v-model="pagination" :total-of-items="items.length" />
+      <Pagination v-model="pagination" :total-of-items="itemLength" />
       <div pt1>{{pagination}}</div>
     </div>
   `,
-    data: () => ({ headers, items, itemLength: items.length, pagination: { currentPage: 1, itemPerPage: 20 } })
+    data: () => ({ itemLength: 90, pagination: { currentPage: 1, itemPerPage: 20 } })
   }))
   .add('Hide Selection', () => ({
     components: { Pagination },
     template: `
     <div class="container">
-      <Pagination v-model="pagination" :total-of-items="items.length" hide-selection/>
+      <Pagination v-model="pagination" :total-of-items="itemLength" hide-select/>
       <div pt1>{{pagination}}</div>
     </div>
   `,
-    data: () => ({ headers, items, itemLength: items.length, pagination: { currentPage: 1, itemPerPage: 20 } })
-  }))
-  .add('Hide pagination items page', () => ({
-    components: { Pagination },
-    template: `
-    <div class="container">
-      <Pagination v-model="pagination" :total-of-items="items.length" hide-items/>
-      <div pt1>{{pagination}}</div>
-    </div>
-  `,
-    data: () => ({ headers, items, itemLength: items.length, pagination: { currentPage: 1, itemPerPage: 20 } })
+    data: () => ({ itemLength: items.length, pagination: { currentPage: 1, itemPerPage: 50 } })
   }))
