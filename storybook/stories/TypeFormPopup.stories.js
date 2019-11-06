e4997f3e25ad668aeb2cd15165104d4afd5936ba
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-06 09:14:07 +0700

    add a tag example on storybook

--- storybook/stories/TypeFormPopup.stories.js
+++ storybook/stories/TypeFormPopup.stories.js
@@ -6,15 +6,26 @@ import Button from '@mesg-components/button'
 storiesOf('TypeFormPopup', module)
   .add('default', () => ({
     components: { TypeFormPopup },
-    template: `<TypeFormPopup id="bOJ0qz">TEST</TypeFormPopup>`
+    template: `
+    <div class="container">
+      <TypeFormPopup id="bOJ0qz">TEST</TypeFormPopup>
+    </div>`
   }))
-
   .add('Button', () => ({
     components: { TypeFormPopup, Button },
     template: `
     <div class="container">
       <TypeFormPopup id="bOJ0qz">
         <Button primary>TEST</Button>
+      </TypeFormPopup>
+    </div>`
+  }))
+  .add('a tag', () => ({
+    components: { TypeFormPopup },
+    template: `
+    <div class="container">
+      <TypeFormPopup id="bOJ0qz">
+        <a primary>TEST</a>
       </TypeFormPopup>
     </div>`
   }))
