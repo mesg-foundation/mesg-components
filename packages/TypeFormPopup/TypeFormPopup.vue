6d3710a726d7b60d7fcd708559fe689f1740d895
Emmanuel Drouin <emmanuel@mesg.tech>
2019-11-06 11:26:40 +0700

    Add a:hover in style

--- packages/TypeFormPopup/TypeFormPopup.vue
+++ packages/TypeFormPopup/TypeFormPopup.vue
@@ -6,49 +6,59 @@
 
 <script>
 export default {
-  name: 'TypeFormPopup',
+  name: "TypeFormPopup",
   props: {
     id: { type: String, required: true },
-    url: { type: String, default: 'https://mesgfoundation.typeform.com/to' }
+    url: { type: String, default: "https://mesgfoundation.typeform.com/to" }
   },
   data() {
     return {
       openPopup: undefined
-    }
+    };
   },
   mounted() {
     if (!!window.typeformEmbed) {
-      this.createTypeFormButton()
+      this.createTypeFormButton();
     } else {
-      const self = this
+      const self = this;
       window.typeformEmbed = (function(d, s, id) {
         var js,
-          fjs = d.getElementsByTagName(s)[0]
-        var t = window.typeformEmbed || {}
-        if (d.getElementById(id)) return t
-        js = d.createElement(s)
-        js.id = id
-        js.src = 'https://embed.typeform.com/embed.js'
+          fjs = d.getElementsByTagName(s)[0];
+        var t = window.typeformEmbed || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://embed.typeform.com/embed.js";
         js.onload = () => {
-          self.createTypeFormButton()
-        }
-        fjs.parentNode.insertBefore(js, fjs)
-        return t
-      })(document, 'script', 'typef_orm_share')
+          self.createTypeFormButton();
+        };
+        fjs.parentNode.insertBefore(js, fjs);
+        return t;
+      })(document, "script", "typef_orm_share");
     }
   },
   methods: {
     createTypeFormButton() {
       this.$nextTick(() => {
-        this.openPopup = window.typeformEmbed.makePopup(`${this.url}/${this.id}`, {
-          mode: 'popup',
-          autoClose: 0
-        })
-      })
+        this.openPopup = window.typeformEmbed.makePopup(
+          `${this.url}/${this.id}`,
+          {
+            mode: "popup",
+            autoClose: 0
+          }
+        );
+      });
     },
     onClickedPopup() {
-      this.openPopup.open()
+      this.openPopup.open();
     }
   }
-}
+};
 </script>
+
+
+<style lang="scss" scoped>
+a:hover {
+  cursor: pointer;
+}
+</style>
