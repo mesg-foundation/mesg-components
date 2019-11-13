a60ab409e9df3e4ec529fe856dfb528a52e19ea9
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-13 10:06:48 +0700

    add image and update Header component example on storybook

--- storybook/stories/Header.stories.js
+++ storybook/stories/Header.stories.js
@@ -2,27 +2,37 @@ import { storiesOf } from '@storybook/vue'
 import Header from '@mesg-components/header'
 import Button from '@mesg-components/button'
 
+import image from '../assets/img/home.svg'
+
 storiesOf('Header', module)
   .add('default', () => ({
     components: { Header, Button },
-    template: `
-  <Header :title="title" :description="description">
-    <div>
-      <Button target="_blank" primary>Get started</Button>
-    </div>
-  </Header>`,
+    template: `<Header :title="title" :description="description" :image="image"/>`,
     data: () => ({
       title: 'The builders’ open economy',
-      description: 'A decentralized event-driven task-orchestration system based on an open market of pay-per-use services.'
+      description: 'A decentralized event-driven task-orchestration system based on an open market of pay-per-use services.',
+      image
     })
   }))
-  .add('default without image', () => ({
+  .add('Header with a slot to describe', () => ({
     components: { Header, Button },
     template: `
-    <Header :title="title" :description="description" no-picture>
-      <div>
-        <Button target="_blank" primary>Get started</Button>
-      </div>
+    <Header :title="title" :image="image">
+      <p>{{description}}</p>
+      <Button primary>xxx</Button>
+    </Header>`,
+    data: () => ({
+      title: 'The builders’ open economy',
+      description: 'A decentralized event-driven task-orchestration system based on an open market of pay-per-use services.',
+      image
+    })
+  }))
+  .add('No picture, header in full width', () => ({
+    components: { Header, Button },
+    template: `
+    <Header :title="title">
+      <p>{{description}}</p>
+      <Button primary>xxx</Button>
     </Header>`,
     data: () => ({
       title: 'The builders’ open economy',
