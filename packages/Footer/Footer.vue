58e6e992d562ab223c796a3061d5fd77aaa4a449
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-13 15:39:34 +0700

    update footer components

--- packages/Footer/Footer.vue
+++ packages/Footer/Footer.vue
@@ -1,30 +1,27 @@
 <template>
   <section id="footer">
-    <div class="container">
-      <nav flex row space-between wrap mobile-column-reverse>
-        <div flex column third>
-          <a href="/">
-            <img v-if="isImage" :src="banner" alt=" " />
-            <h2 v-else>{{banner}}</h2>
+    <nav>
+      <div id="banner-box">
+        <a href="/">
+          <img v-if="isImage" :src="banner" alt=" " />
+          <h2 v-else>{{ banner }}</h2>
+        </a>
+        <p v-if="copyright" class="copyright">{{ copyright }}</p>
+        <a v-if="policy" :href="policy.link" class="policy link-secondary">{{ policy.text }}</a>
+        <div v-if="icons" id="list-icon">
+          <a v-for="(icon, i) in icons" :key="i" :href="icon.to" target="_blank" :class="{ icon: icon }">
+            <i :class="icon.classIcon"></i>
           </a>
-          <p v-if="copyRight" class="copyright">{{copyRight}}</p>
-          <a v-if="policy" :href="policy.link" class="policy link-secondary" mb1>{{policy.title}}</a>
-          <div v-if="icons.length" flex space-between wrap>
-            <a v-for="(icon,i) in icons" :key="i" :href="icon.to" target="_blank" :class="icon">
-              <i :class="icon.icon"></i>
-            </a>
-          </div>
         </div>
-        <ul flex row mobile-column class="menu">
-          <li flex column quarter v-for="(link, i) in links" :key="i">
-            <div v-for="(detail,j) in link" :key="j" :mb1="!isLast(link,i)">
-              <a v-if="isExternalLink(detail.to)" :href="detail.to" :class="{category:detail.category,'link-secondary': !detail.category}" target="_blank">{{detail.title}}</a>
-              <nuxt-link v-else :to="detail.to" :class="{category:detail.category,'link-secondary': !detail.category }">{{detail.title}}</nuxt-link>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </div>
+      </div>
+      <ul id="list-categories">
+        <li v-for="(category, i) in categories" :key="i">
+          <template v-for="item in items">
+            <slot v-if="item.key === category" :name="item.key" :item="item">{{ item[category] }}</slot>
+          </template>
+        </li>
+      </ul>
+    </nav>
   </section>
 </template>
 
@@ -32,20 +29,12 @@
 export default {
   name: 'Footer',
   props: {
-    banner: { type: String },
-    copyRight: { type: String },
+    banner: { type: String, require: true },
+    categories: { type: Array },
+    copyright: { type: String },
     policy: { type: Object },
-    links: { type: Array },
-    icons: { type: Array, default: [] }
-  },
-  methods: {
-    isLast(links, i) {
-      const value = links.length - 1 === i
-      return value
-    },
-    isExternalLink(link) {
-      return /http(s)?/i.test(link)
-    }
+    items: { type: Array },
+    icons: { type: Array }
   },
   computed: {
     isImage() {
@@ -57,12 +46,30 @@ export default {
 
 <style lang="scss" scoped>
 @import '@mesg-components/theme/_variables';
-@import '@mesg-components/theme/_structure';
 
 #footer {
   padding: calc(#{$margin}* 3);
-  width: 100%;
   background-color: $light-grey;
+}
+#list-categories {
+  min-width: 75%;
+  display: flex;
+  justify-content: space-around;
+}
+#list-icon {
+  padding-top: $margin;
+}
+
+#banner-box {
+  display: flex;
+  flex-direction: column;
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  max-width: $width;
+  margin: auto;
 }
 
 a {
@@ -70,19 +77,11 @@ a {
 }
 
 ul {
+  list-style: none;
+  margin: 0;
   padding: 0;
 }
 
-.outline {
-  border: solid 1px $primary-light;
-  background-color: $primary-very-dark;
-}
-.purple {
-  color: $primary-light;
-}
-.white {
-  color: $white;
-}
 .copyright {
   font-size: 12px;
   font-weight: normal;
@@ -92,6 +91,7 @@ ul {
   letter-spacing: normal;
   color: $dark-grey;
 }
+
 .policy {
   font-size: 12px !important;
   font-weight: bold;
@@ -101,16 +101,7 @@ ul {
   letter-spacing: normal;
   color: $dark-grey;
 }
-.category {
-  font-size: 17px;
-  font-weight: 600;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: $primary-dark;
-  display: inline;
-}
+
 .icon {
   font-size: 18px;
   font-weight: normal;
@@ -118,11 +109,15 @@ ul {
   font-stretch: normal;
   line-height: normal;
   color: $dark-grey;
+  &:hover {
+    opacity: 0.7;
+    transition: 0.1s ease;
+  }
+  &:not(:first-child) {
+    padding-left: 10px;
+  }
 }
-.icon:hover {
-  opacity: 0.7;
-  transition: 0.1s ease;
-}
+
 .link-secondary {
   font-size: 15px;
   font-weight: normal;
@@ -146,11 +141,16 @@ img:hover {
     padding: calc(#{$margin} * 2);
     padding-top: 0;
   }
-  .container {
-    padding: 0;
-  }
 }
 @media only screen and (max-width: $mobile-breakpoint) {
+  nav {
+    flex-direction: column-reverse;
+  }
+
+  #list-categories {
+    flex-direction: column;
+  }
+
   a.link {
     min-height: auto;
     margin-bottom: 0;
