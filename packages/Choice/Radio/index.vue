2e16464d313bcb3fffb54883ce948b120d01ba7c
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-14 09:08:15 +0700

    Update radio button style

--- packages/Choice/Radio/index.vue
+++ packages/Choice/Radio/index.vue
@@ -2,15 +2,17 @@
   <div>
     <ul>
       <li v-for="option in options" :key="option.key">
-        <input
-          type="radio"
-          :id="option.key"
-          :value="option.value"
-          :checked="checkValue(option.value)"
-          @input="$emit('input', option.value)"
-          @change="$emit('change', $event.target.checked)"
-        />
-        <label>{{option.value}}</label>
+        <div class="radio">
+          <input
+            type="radio"
+            :id="option.key"
+            :value="option.value"
+            :checked="checkValue(option.value)"
+            @input="$emit('input', option.value)"
+            @change="$emit('change', $event.target.checked)"
+          />
+          <label class="radio-label" :for="option.key">{{option.value}}</label>
+        </div>
       </li>
     </ul>
   </div>
@@ -29,4 +31,63 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@mesg-components/theme/_variables';
+
+.radio {
+  margin: 0.5rem;
+  input[type='radio'] {
+    position: absolute;
+    opacity: 0;
+    + .radio-label {
+      &:before {
+        content: '';
+        background: $light-grey;
+        border-radius: 100%;
+        border: 1px solid darken($light-grey, 25%);
+        display: inline-block;
+        width: 1.4em;
+        height: 1.4em;
+        position: relative;
+        top: -0.2em;
+        margin-right: 1em;
+        vertical-align: top;
+        cursor: pointer;
+        text-align: center;
+        transition: all 250ms ease;
+      }
+    }
+    &:checked {
+      + .radio-label {
+        &:before {
+          background-color: $primary-very-light;
+          box-shadow: inset 0 0 0 4px $light-grey;
+        }
+      }
+    }
+    &:focus {
+      + .radio-label {
+        &:before {
+          outline: none;
+          border-color: $primary-very-light;
+        }
+      }
+    }
+    &:disabled {
+      + .radio-label {
+        &:before {
+          box-shadow: inset 0 0 0 4px $light-grey;
+          border-color: darken($light-grey, 25%);
+          background: darken($light-grey, 25%);
+        }
+      }
+    }
+    + .radio-label {
+      &:empty {
+        &:before {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+}
 </style>
