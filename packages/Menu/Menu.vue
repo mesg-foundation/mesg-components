d386947fe832c49dcdf45fcec3cebfd79ef9e30d
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-15 17:05:37 +0700

    fix variables style on menu component

--- packages/Menu/Menu.vue
+++ packages/Menu/Menu.vue
@@ -1,26 +1,28 @@
 <template>
-  <nav>
-    <div class="banner-box">
-      <div>
-        <a>
-          <img :src="banner" alt="" />
+  <div>
+    <nav flex space-between align-center>
+      <div flex row space-between align-center>
+        <div>
+          <a>
+            <img :src="banner" alt="" />
+          </a>
+        </div>
+        <a @click="open = !open" class="burger" hidden-desktop>
+          <span v-if="open">&times;</span>
+          <span v-else>☰</span>
         </a>
       </div>
-      <a @click="open = !open" class="burger" hidden-desktop>
-        <span v-if="open">&times;</span>
-        <span v-else>☰</span>
-      </a>
-    </div>
-    <div class="actions" :class="{ open }">
-      <ul>
-        <li v-for="(category, i) in categories" :key="i">
-          <template v-for="item in items">
-            <slot v-if="item.key === category" :name="item.key" :item="item">{{ item.key }}</slot>
-          </template>
-        </li>
-      </ul>
-    </div>
-  </nav>
+      <div flex row space-between class="actions" :class="{ open }">
+        <ul flex row mobile-column align-center>
+          <li v-for="(category, i) in categories" :key="i" flex column align-center>
+            <template v-for="item in items">
+              <slot v-if="category === item.key" :name="item.key" :item="item">{{ item.key }}</slot>
+            </template>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -46,44 +48,19 @@ export default {
 
 <style lang="scss" scoped>
 @import '@mesg-components/theme/_variables';
+@import '@mesg-components/theme/_structure';
 
 nav {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
   height: 80px;
   padding: calc(#{$margin} - 2px) calc(#{$margin} * 2);
-}
-img {
-  width: 100%;
-  max-width: 153px !important;
-  max-height: 40px !important;
-  &:hover {
-    opacity: 0.7;
-    transition: 0.1s ease;
-  }
-}
-
-.banner-box {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-}
-
-.actions {
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
-  ul {
-    display: flex;
-    justify-content: space-between;
-    li {
-      display: flex;
-      align-items: center;
+  img {
+    min-width: 153px;
+    max-width: 153px;
+    min-height: 40px;
+    max-height: 40px;
+    &:hover {
+      opacity: 0.7;
+      transition: 0.1s ease;
     }
   }
 }
@@ -94,47 +71,8 @@ img {
     max-width: 100%;
     display: inline-block;
   }
-}
-
-//menu responsive//
-@media only screen and (max-width: $tablet-breakpoint) {
-  .actions {
-    border-radius: 6px;
-    display: none;
-    position: absolute;
-    left: 2.5%;
-    width: 95%;
-    top: 80px;
-    z-index: 1;
-    background-color: var(--white);
-    box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2);
-    &.open {
-      display: block;
-      animation: appear-in 0.2s ease-in;
-    }
-  }
-  .burger {
-    text-align: right;
-    display: inline-block;
-    line-height: 44px;
-    height: 44px;
-    font-size: 2em;
-    &:hover {
-      cursor: pointer;
-    }
-  }
-}
-//menu mobile//
-@media only screen and (max-width: $mobile-breakpoint) {
-  ul {
-    align-items: left;
-  }
   li {
-    margin-bottom: var(--margin);
-    a:not(.btn--primary) {
-      line-height: 40px;
-      padding: 0;
-    }
+    text-align: center;
   }
 }
 </style>
--- storybook/stories/Menu.stories.js
+++ storybook/stories/Menu.stories.js
@@ -6,8 +6,26 @@ import logo from '../assets/img/MESG-logo-horizontal-purple.svg'
 
 const categories = ['technology', 'developers', 'showcase', 'contributions', 'token', 'get-started']
 const items = [
-  { key: 'technology', text: 'Technology' },
-  { key: 'developers', text: 'Developers' },
+  {
+    key: 'technology',
+    text: 'Technology',
+    child: [
+      { text: 'Orchestrator', to: '' },
+      { text: 'SDK', to: '' },
+      { text: 'Marketplace', to: '' },
+      { text: 'Enterprise', to: '' }
+    ]
+  },
+  {
+    key: 'developers',
+    text: 'Developers',
+    child: [
+      { text: 'Orchestrator', to: '' },
+      { text: 'SDK', to: '' },
+      { text: 'Marketplace', to: '' },
+      { text: 'Enterprise', to: '' }
+    ]
+  },
   { key: 'showcase', text: 'Showcase' },
   { key: 'contributions', text: 'Contributions' },
   { key: 'token', text: 'Token' },
@@ -20,27 +38,54 @@ storiesOf('Menu', module).add('default', () => ({
   <Menu :banner="banner" :categories="categories" :items="items">
 
     <template v-slot:technology="{ item }">
-      <p>{{item.text}}</p>
+      <div class="drop-down">
+        <a href="#" class="top-menu">
+          {{ item.text }}
+          <i class="far fa-angle-down">
+          </i>
+        </a>
+        <div flex column class="sub-menu">
+          <a v-for="(link,i) in item.child" :key="i">
+            {{link.text}}
+          </a>
+        </div>
+      </div>
     </template>
 
     <template v-slot:developers="{ item }">
-      <p>{{item.text}}</p>
+      <div class="drop-down">
+        <a href="#" class="top-menu">
+          {{ item.text }}
+          <i class="far fa-angle-down"/>
+        </a>
+        <div flex column class="sub-menu" >
+          <a v-for="(link,i) in item.child" :key="i">
+            {{link.text}}
+          </a>
+        </div>
+      </div>
     </template>
 
-    <template v-slot:show="{ item }">
-      <p>{{item.text}}</p>
+    <template v-slot:showcase="{ item }">
+      <a href="#" class="top-menu">
+        {{ item.text }}
+      </a>
     </template>
 
     <template v-slot:contributions="{ item }">
-      <p>{{item.text}}</p>
+      <a href="#" class="top-menu">
+        {{ item.text }}
+      </a>  
     </template>
 
     <template v-slot:token="{ item }">
-        <p>{{item.text}}</p>
+      <a href="#" class="top-menu">
+        {{ item.text }}
+      </a>  
     </template>
 
     <template v-slot:get-started="{ item }">
-      <Button primary>{{item.text}}</Button>
+      <Button primary small>{{item.text}}</Button>
     </template>
     
   </Menu>`,
