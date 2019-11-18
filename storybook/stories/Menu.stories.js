bb49852ca4ed0143a539b1439fecfd08c83d39fb
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-18 16:31:58 +0700

    update menu storybook

--- storybook/stories/Menu.stories.js
+++ storybook/stories/Menu.stories.js
@@ -4,14 +4,13 @@ import Button from '@mesg-components/button'
 
 import logo from '../assets/img/MESG-logo-horizontal-purple.svg'
 
-import '../assets/menu.scss'
-
 const categories = ['technology', 'developers', 'showcase', 'contributions', 'token', 'get-started']
 const items = [
   {
     key: 'technology',
     text: 'Technology',
-    child: [
+    to: '',
+    subMenu: [
       { text: 'Orchestrator', to: '' },
       { text: 'SDK', to: '' },
       { text: 'Marketplace', to: '' },
@@ -21,7 +20,8 @@ const items = [
   {
     key: 'developers',
     text: 'Developers',
-    child: [
+    to: '',
+    subMenu: [
       { text: 'Orchestrator', to: '' },
       { text: 'SDK', to: '' },
       { text: 'Marketplace', to: '' },
@@ -31,65 +31,12 @@ const items = [
   { key: 'showcase', text: 'Showcase' },
   { key: 'contributions', text: 'Contributions' },
   { key: 'token', text: 'Token' },
-  { key: 'get-started', text: 'Get Started' }
+  { key: 'get-started', text: 'Get Started', type: 'button' }
 ]
 
 storiesOf('Menu', module).add('default', () => ({
   components: { Menu, Button },
   template: `
-  <Menu :banner="banner" :categories="categories" :items="items">
-
-    <template v-slot:technology="{ item }">
-      <div class="drop-down">
-        <a href="#" class="top-menu">
-          {{ item.text }}
-          <i class="far fa-angle-down">
-          </i>
-        </a>
-        <div flex column class="sub-menu">
-          <a v-for="(link,i) in item.child" :key="i">
-            {{link.text}}
-          </a>
-        </div>
-      </div>
-    </template>
-
-    <template v-slot:developers="{ item }">
-      <div class="drop-down">
-        <a href="#" class="top-menu">
-          {{ item.text }}
-          <i class="far fa-angle-down"/>
-        </a>
-        <div flex column class="sub-menu" >
-          <a v-for="(link,i) in item.child" :key="i">
-            {{link.text}}
-          </a>
-        </div>
-      </div>
-    </template>
-
-    <template v-slot:showcase="{ item }">
-      <a href="#" class="top-menu">
-        {{ item.text }}
-      </a>
-    </template>
-
-    <template v-slot:contributions="{ item }">
-      <a href="#" class="top-menu">
-        {{ item.text }}
-      </a>  
-    </template>
-
-    <template v-slot:token="{ item }">
-      <a href="#" class="top-menu">
-        {{ item.text }}
-      </a>  
-    </template>
-    
-    <template v-slot:get-started="{ item }">
-      <Button primary small>{{item.text}}</Button>
-    </template>
-
-  </Menu>`,
-  data: () => ({ banner: logo, items, categories })
+  <Menu :logo="logo" :items="items"/>`,
+  data: () => ({ logo, items })
 }))
