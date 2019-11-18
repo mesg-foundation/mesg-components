ea1d7adf9825957aaf1dab4e7bff2d41cf8ed1e1
Benz <ruthamnong.thotsaphon@gmail.com>
2019-11-18 13:32:51 +0700

    add show show and hide at type password

--- packages/TextField/TextField.vue
+++ packages/TextField/TextField.vue
@@ -1,29 +1,47 @@
 <template>
   <label>
     {{label}}
-    <input
-      :type="type"
-      :placeholder="placeholder"
-      :value="value"
-      @input="$emit(`input`, $event.target.value)"
-    />
+    <div class="password">
+      <template v-if="type==='password'">
+        <button @click="showPwd" :class="{actived: showPassword}">
+          <i v-if="showPassword" class="fas fa-eye"></i>
+          <i v-else-if="!showPassword" class="fas fa-eye-slash"></i>
+        </button>
+      </template>
+      <input :type="textType" :placeholder="placeholder" :value="value" @input="$emit(`input`, $event.target.value)" />
+    </div>
   </label>
 </template>
 
 <script>
 export default {
-  name: "TextField",
+  name: 'TextField',
   props: {
-    type: { type: String, default: "text" },
+    type: { type: String, default: 'text' },
     label: { type: String, required: true },
     placeholder: { type: String, required: true },
     value: { type: String, required: true }
+  },
+  mounted() {
+    this.textType = this.type
+  },
+  data() {
+    return {
+      textType: 'password',
+      showPassword: false
+    }
+  },
+  methods: {
+    showPwd() {
+      this.showPassword = !this.showPassword
+      this.textType = this.showPassword ? 'text' : 'password'
+    }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
-@import "@mesg-components/theme/_variables";
+@import '@mesg-components/theme/_variables';
 
 label {
   font-size: 15px;
@@ -33,20 +51,35 @@ label {
   line-height: normal;
   letter-spacing: normal;
   color: $primary-dark;
-}
-
-input {
-  color: $dark-grey;
-  font-size: 15px;
-  padding: 15px $margin;
-  border-radius: 3px;
-  border: solid 1px $primary-very-light;
-  background-color: $white;
-  width: 100%;
-  margin-top: $margin;
-  &:focus {
-    outline: none;
-    box-shadow: 0 0 0 1px $primary-very-light inset;
+  .password {
+    position: relative;
+    button {
+      border: none;
+      outline: none;
+      position: absolute;
+      right: $margin;
+      top: 50%;
+      opacity: 0.3;
+      color: $dark-grey;
+      cursor: pointer;
+    }
+    .actived {
+      opacity: 1;
+    }
+    input {
+      color: $dark-grey;
+      font-size: 15px;
+      padding: 15px $margin;
+      border-radius: 3px;
+      border: solid 1px $primary-very-light;
+      background-color: $white;
+      width: 100%;
+      margin-top: $margin;
+      &:focus {
+        outline: none;
+        box-shadow: 0 0 0 1px $primary-very-light inset;
+      }
+    }
   }
 }
 </style>
